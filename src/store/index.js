87331fd9d29e44e65e52e5cbba7978291c688000
jiunlee19 <tearofglass@naver.com>
2025-08-20 20:02:02 +0900

    initialState 설정, 임시 데이터 작성

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+const TODOS : [{
+  id:1
+  year:'',
+  month:'',
+  day:'',
+  isDone:false,
+  cycle:{year:false, month:false, day:false},
+  text:'',
+},{
+  id:2
+  year:'',
+  month:'',
+  day:'',
+  isDone:false,
+  cycle:{year:false, month:false, day:false},
+  text:'',
+}]
+
+const initialState = {todos: []};
