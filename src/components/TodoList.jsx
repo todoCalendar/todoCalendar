2b4e747338bd906f4855fdc78f9b7a27a50b66c7
gimeast <slasnrndu@gmail.com>
2025-08-21 11:37:19 +0900

    feat: TodoList 컴포넌트 마크업 구현
    - TodoList 컴포넌트에 기본 UI 구조 추가
    - 할일 목록, 체크박스, 삭제 버튼 마크업 완성

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React from "react";
 
 const TodoList = () => {
     return (
-        <div>
+        <aside>
+            <h3>TODO LIST</h3>
+            {/*필터 컴포넌트 추가되는 영역*/}
 
-        </div>
+            <button>선택삭제</button>
+            <button>전체삭제</button>
+            <ul>
+                <li>
+                    <span>1</span>
+                    <div>
+                        <input type="checkbox" /> 청소하기
+                    </div>
+                    <div>
+                        <input type="checkbox" /> 설거지하기
+                    </div>
+                </li>
+                <li>
+                    <span>2</span>
+                    <div>
+                        <input type="checkbox" /> 청소하기
+                    </div>
+                    <div>
+                        <input type="checkbox" /> 설거지하기
+                    </div>
+                </li>
+                <li>
+                    <span>3</span>
+                    <div>
+                        <input type="checkbox" /> 청소하기
+                    </div>
+                    <div>
+                        <input type="checkbox" /> 설거지하기
+                    </div>
+                </li>
+                <li>
+                    <span>4</span>
+                    <div>
+                        <input type="checkbox" /> 청소하기
+                    </div>
+                    <div>
+                        <input type="checkbox" /> 설거지하기
+                    </div>
+                </li>
+                <li>
+                    <span>5</span>
+                    <div>
+                        <input type="checkbox" /> 청소하기
+                    </div>
+                    <div>
+                        <input type="checkbox" /> 설거지하기
+                    </div>
+                </li>
+            </ul>
+        </aside>
     );
 };
 
