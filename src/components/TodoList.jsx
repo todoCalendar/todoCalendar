37ca4ca71ba057d9983fa8c2acaf03d1560fce7f
gimeast <slasnrndu@gmail.com>
2025-08-21 23:59:37 +0900

    feat: TodoList 삭제 기능 구현
    - 체크박스 선택 상태 관리 추가
    - 선택삭제/전체삭제 버튼 이벤트 핸들러 연결
    - todo 체크박스 선택/해제 기능 구현

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,57 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TodoList = () => {
+    const [selectedIds, setSelectedIds] = useState(new Set());
+
+    function handleDeleteSelected() {
+        //todo: 선택삭제 dispatch
+        setSelectedIds(new Set());
+    }
+    function handleDeleteAll() {
+        //todo: 전체삭제 dispatch
+        setSelectedIds(new Set());
+    }
+
+    function toggleSelected(id) {
+        setSelectedIds((prevState) => {
+            const next = new Set(prevState);
+
+            if (prevState.has(id)) {
+                next.delete(id);
+            } else {
+                next.add(id);
+            }
+
+            return next;
+        });
+    }
+
+    useEffect(() => {
+        console.log(selectedIds);
+    }, [selectedIds]);
+
     return (
         <aside>
             <h3>TODO LIST</h3>
             {/*필터 컴포넌트 추가되는 영역*/}
 
-            <button>선택삭제</button>
-            <button>전체삭제</button>
+            <button onClick={handleDeleteSelected}>선택삭제</button>
+            <button onClick={handleDeleteAll}>전체삭제</button>
             <ul>
                 <li>
                     <span>1</span>
                     <div>
-                        <input type="checkbox" /> 청소하기
+                        <input
+                            id="checkbox1"
+                            type="checkbox"
+                            onChange={() => toggleSelected(1)}
+                        />
+                        <label htmlFor="checkbox1">청소하기</label>
                     </div>
                     <div>
-                        <input type="checkbox" /> 설거지하기
-                    </div>
-                </li>
-                <li>
-                    <span>2</span>
-                    <div>
-                        <input type="checkbox" /> 청소하기
-                    </div>
-                    <div>
-                        <input type="checkbox" /> 설거지하기
-                    </div>
-                </li>
-                <li>
-                    <span>3</span>
-                    <div>
-                        <input type="checkbox" /> 청소하기
-                    </div>
-                    <div>
-                        <input type="checkbox" /> 설거지하기
-                    </div>
-                </li>
-                <li>
-                    <span>4</span>
-                    <div>
-                        <input type="checkbox" /> 청소하기
-                    </div>
-                    <div>
-                        <input type="checkbox" /> 설거지하기
-                    </div>
-                </li>
-                <li>
-                    <span>5</span>
-                    <div>
-                        <input type="checkbox" /> 청소하기
-                    </div>
-                    <div>
-                        <input type="checkbox" /> 설거지하기
+                        <input
+                            id="checkbox2"
+                            type="checkbox"
+                            onChange={() => toggleSelected(2)}
+                        />
+                        <label htmlFor="checkbox2">설거지하기</label>
                     </div>
                 </li>
             </ul>
