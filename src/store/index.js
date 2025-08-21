d78b0b8a2e9671077fbb1b38b9349bd54916a315
jiunlee19 <tearofglass@naver.com>
2025-08-21 10:06:07 +0900

    store 세팅

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const TODOS : [{
   id:1
   year:'',
@@ -17,3 +19,56 @@ const TODOS : [{
 }]
 
 const initialState = {todos: []};
+
+// 액션 타입 상수
+export const SELECT_TODAY_TODO = "SELECT_TODAY_TODO";
+export const SELECT_MONTH_TODO = "SELECT_MONTH_TODO";
+export const ADD_TODO = "ADD_TODO";
+export const TODO_DONE = "TODO_DONE";
+export const DELETE_TODO = "DELETE_TODO";
+export const DELETE_ALL = "DELETE_ALL";
+export const FILTER_TODO = "FILTER_TODO";
+
+// 액션 생성자
+export const selectTodayTodo = (year, month, day)=>({
+  type:SELECT_TODAY_TODO, payload:day 
+})
+//선택한 날짜 TODO LIST
+export const selectMonthTodo = (year, month)=>({
+  type:SELECT_MONTH_TODO
+})
+//MONTHLY TODO LIST
+export const addTodo = (todo)=>({
+  type:ADD_TODO
+})
+export const todoDone = (id)=>({
+  type:TODO_DONE
+})
+export const deleteTodo = (id)=>({
+  type:DELETE_TODO
+})
+export const deleteAll = (year, month)=>({
+  type:DELETE_ALL
+})
+
+// 마지막에 추가할 기능
+export const filterTodo = ()=>({
+  type:FILTER_TODO
+})
+
+// 리듀서
+export default function reducer(state = initialState, action){
+  switch(action.type){
+    case SELECT_TODAY_TODO:
+    case SELECT_MONTH_TODO:
+    case ADD_TODO:
+    case TODO_DONE:
+    case DELETE_TODO:
+    case DELETE_ALL:
+    case FILTER_TODO:
+    default :
+      return state;
+  }
+}
+
+export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
