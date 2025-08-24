75ecfb84536551c2b734fefee71596dd8a95901a
silverstar9482 <silverstar9482@gmail.com>
2025-08-25 01:26:53 +0900

    import 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-// import Calendar from "./components/Calendar";
-import InputTodo from "./components/InputTodo";
+import Calendar from "./components/Calendar";
+// import InputTodo from "./components/InputTodo";
 // import TodayTodo from "./components/TodayTodo";
-// import TodoList from "./components/TodoList";
+import TodoList from "./components/TodoList";
 
 function App() {
     return (
         <>
-            {/* <Calendar /> */}
-            {/* <TodayTodo /> */}
-            <InputTodo />
-            {/* <TodoList /> */}
+            <Calendar />
+            {/* <TodayTodo />
+            <InputTodo /> */}
+            <TodoList />
         </>
     );
 }
