96a9b6847ec58cbd3daa3bf4ccdddc8d54069406
jiunlee19 <tearofglass@naver.com>
2025-08-25 02:35:15 +0900

    필터 버튼 관련 수정

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,120 +1,12 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { useSelector } from "react-redux";
 
 export default function Calendar() {
-    const dummy = [
-        {
-            id: 1,
-            year: "2025",
-            month: "08",
-            day: "01",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "청소하기",
-        },
-        {
-            id: 2,
-            year: "2025",
-            month: "08",
-            day: "01",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "빨래하기",
-        },
-        {
-            id: 3,
-            year: "2025",
-            month: "08",
-            day: "02",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "밥하기",
-        },
-        {
-            id: 4,
-            year: "2025",
-            month: "08",
-            day: "03",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "청소하기",
-        },
-        {
-            id: 5,
-            year: "2025",
-            month: "08",
-            day: "04",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "빨래하기",
-        },
-        {
-            id: 6,
-            year: "2025",
-            month: "08",
-            day: "05",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "밥하기",
-        },
-        {
-            id: 7,
-            year: "2025",
-            month: "08",
-            day: "06",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "청소하기",
-        },
-        {
-            id: 8,
-            year: "2025",
-            month: "08",
-            day: "07",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "빨래하기",
-        },
-        {
-            id: 9,
-            year: "2025",
-            month: "08",
-            day: "08",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "밥하기",
-        },
-        {
-            id: 10,
-            year: "2025",
-            month: "08",
-            day: "09",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "청소하기",
-        },
-        {
-            id: 11,
-            year: "2025",
-            month: "08",
-            day: "10",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "빨래하기",
-        },
-        {
-            id: 12,
-            year: "2025",
-            month: "08",
-            day: "11",
-            isDone: false,
-            cycle: { year: false, month: false, day: false },
-            text: "밥하기",
-        },
-    ];
+    const todos = useSelector((state) => state.todos);
 
-    const calendarEvents = dummy.map((todo) => ({
+    const calendarEvents = todos.map((todo) => ({
         title: todo.text,
         start: `${todo.year}-${todo.month}-${todo.day}`,
         id: todo.id,
