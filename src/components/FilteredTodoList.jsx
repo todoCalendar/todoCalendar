964ba6e2f074851dd9f771602659dd84d6b59498
jiunlee19 <tearofglass@naver.com>
2025-08-26 16:12:07 +0900

    필요없는 주석 삭제

--- src/components/FilteredTodoList.jsx
+++ src/components/FilteredTodoList.jsx
@@ -1,11 +1,10 @@
 import { useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { filterTodo } from "../store/index.js"; // filterTodo 액션 생성자
+import { filterTodo } from "../store/index.js";
 
 export function FilteredTodoList() {
-    const todos = useSelector((state) => state.todos); //콜백함수라서 이렇게 불러와야됨
+    const todos = useSelector((state) => state.todos);
 
-    // filter, map,
     const filters = useMemo(() => {
         return new Set(todos.map((todo) => todo.text));
     });
