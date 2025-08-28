2eb7be69bf0fffa85dcba2deb5d6d32946068701
silverstar9482 <silverstar9482@gmail.com>
2025-08-28 21:00:23 +0900

    병합충돌 수정

--- src/components/FilteredTodoList.jsx
+++ src/components/FilteredTodoList.jsx
@@ -7,13 +7,11 @@ import React, {
 } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { filterTodo } from "../store/index.js";
-import styles from "../assets/css/todo-list.module.css";
 import styles from "../assets/css/filter-btns.module.css";
 
 export function FilteredTodoList() {
     const dispatch = useDispatch();
     const todos = useSelector((state) => state.todos);
-    const activeFilter = useSelector((state) => state.activeFilter);
     const activeFilter = useSelector((state) => state.activeFilter);
 
     const filters = useMemo(() => {
@@ -129,17 +127,4 @@ export function FilteredTodoList() {
             )}
         </div>
     );
-        return new Set(todos.map((todo) => todo.text));
-    });
-    console.log(filters);
-
-    return [...filters].map((filter, i) => (
-        <button
-            key={i}
-            className={`${activeFilter.includes(filter) ? styles.active : ""} ${styles["filter-btn"]}`}
-            onClick={() => dispatch(filterTodo(filter))}
-        >
-            {filter}
-        </button>
-    ));
 }
